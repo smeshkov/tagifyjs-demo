82ed7309fa00a470229528b44defae0b0ecb36ae
smeshkov <smeshkow@gmail.com>
2019-09-29 20:40:19 +1000

    Small code optimisations

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import {Tagify} from 'tagify-js'
 
-console.log('FOOOOO');
-
-const tagify = new Tagify({
+new Tagify({
     target: document.getElementById('tags'),
-    source: 'https://medium.com/@emmanuelmark/how-to-create-a-javascript-library-30df146b3d2a'
+    source: 'https://zoomio.org/posts/id/mock_server-5632006343884800'
 });
