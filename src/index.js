d1e89325ce39ec41b1aa62a254e877e71e0c18bb
smeshkov <smeshkow@gmail.com>
2019-10-05 14:49:02 +1000

    Updated to batch API of version 0.2.0

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import { tagify } from '@smeshkov/tagifyjs';
+import { tagify } from '@zoomio/tagifyjs';
 
 tagify({
-    target: document.getElementById('tags'),
-    source: 'https://zoomio.org/posts/id/mock_server-5632006343884800'
+    appID: 'ff3ee693-acdf-4216-88b7-7b3e4dbe3907',
+    host: 'https://zoomio.org',
+    targets: [
+        {
+            element: document.getElementById('tags'),
+            source: 'https://zoomio.org/posts/id/mock_server-5632006343884800',
+            title: 'Mock server'
+        }
+    ]
 });
