21325bc5d91c435a64b46a6db5f26af3b50016a3
smeshkov <smeshkow@gmail.com>
2020-02-08 19:06:16 +1100

    Updated to support latest version of the TagifyJS

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # TagifyJS Demo
-Demo for [TagifyJS](https://github.com/zoomio/tagifyjs).
+Demo for [Tagify](https://www.zoomio.org/tagify) and [TagifyJS](https://github.com/zoomio/tagifyjs).
 
 ## How to use it
 
-- Run `npm link <path_to_tagifyjs>` to enable dependency.
-- Run `npm i` to install modules.
+- First of all build [TagifyJS](https://github.com/zoomio/tagifyjs) locally: 
+    - go to `tagifyjs` directory (e.g. `cd ../tagifyjs`),
+    - run `npm run build` inside the folder.
+- Go back to the demo directory (e.g. `cd ../tagifyjs-demo`), then run `npm i` to install modules.
+- Run `npm link <path_to_tagifyjs>` (e.g. `npm link ../tagifyjs`) to enable dependency on [TagifyJS](https://github.com/zoomio/tagifyjs).
 - Run `npm start` and open address from console in the browser.
-
--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import tagify from '@zoomio/tagifyjs';
+import tagify, { tagsForAnchors } from '@zoomio/tagifyjs';
 
+// Convenient method for taking care of <a> tags.
+tagsForAnchors({
+    appId: '07553a4b-32cb-466d-9ec6-bebd43c2bcd8',
+    anchorsClassName: 'article-link',
+    targetsClassName: 'tags-1',
+    pagesUrl: 'https://zoomio.org/tagify/relevant'
+});
+
+// Core method, provides more control.
 tagify({
-    appID: 'ff3ee693-acdf-4216-88b7-7b3e4dbe3907',
+    appId: '07553a4b-32cb-466d-9ec6-bebd43c2bcd8',
     host: 'https://zoomio.org',
+    pagesUrl: 'https://zoomio.org/tagify/relevant',
     targets: [
         {
-            element: document.getElementById('tags'),
-            source: 'https://zoomio.org/posts/id/mock_server-5632006343884800',
-            title: 'Mock server'
+            element: document.getElementById('tags-2'),
+            source: 'https://zoomio.org/blog/post/tags_as_a_service-5712840111423488',
+            title: 'Tags as a Service'
         }
     ]
 });
